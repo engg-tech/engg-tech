473d2aebba9c36057744b3d333a5c8e989e454e5
Engg-Tech <info.enggtech@gmail.com>
2026-05-19 10:53:16 +0800

    Update header.js

--- assets/header.js
+++ assets/header.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
                    white-space:nowrap;transition:background 0.2s;flex-shrink:0;"
             onmouseover="this.style.background='#0A2540'"
             onmouseout="this.style.background='#FF6A00'">
-      📞 Call Me Back
+      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="white" style="vertical-align:middle;margin-right:5px;"><path d="M6.6 10.8c1.4 2.8 3.8 5.1 6.6 6.6l2.2-2.2c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.6.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1-9.4 0-17-7.6-17-17 0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1 0 1.3.2 2.5.6 3.6.1.3 0 .7-.2 1L6.6 10.8z"/></svg>Call Me Back
     </button>
 
   </div>
